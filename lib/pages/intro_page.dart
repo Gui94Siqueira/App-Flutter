0277a9501e4f241d6ec63ebbfa06ef67b0b56527
Guilherme <guilherme94siqueira@gmail.com>
2024-09-19 21:20:41 -0300

    first commit

---
+++ lib/pages/intro_page.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class IntroPage extends StatelessWidget {
+  const IntroPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(
+                left: 80.0, right: 80, bottom: 20, top: 40),
+            child: Image.asset("../images/avocado.png"),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(24.0),
+            child: Text(
+              "we deliver groceries at you do doorstap",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 40,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Text(
+            "Fresh items everyday",
+            style: TextStyle(color: Colors.grey[600]),
+          ),
+
+          const Spacer(),
+          Container(
+            decoration: BoxDecoration(
+                color: Colors.deepPurple,
+                borderRadius: BorderRadius.circular(12)
+            ),
+            padding: const EdgeInsets.all(24),
+            child: const Text(
+              "Get Started",
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+
+          const Spacer(),
+        ],
+      ),
+    );
+  }
+}
